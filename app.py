03c0ba9cc93d7c4d850337c70704ac1061da5b0a
AADH4 <118476960+AADH4@users.noreply.github.com>
2025-01-01 23:25:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import textwrap
 import google.generativeai as genai
-
-def to_markdown(text):
-    text = text.replace('•', ' *')
-    return textwrap.indent(text, '> ', predicate=lambda _: True)
 
 genai.configure(api_key='AIzaSyC6X6ttls6_Utpl4SgGtE4XvkbF7CafWDc')
 model = genai.GenerativeModel('gemini-1.5-pro')
@@ -14,5 +9,5 @@ question = st.text_input("Type your query:")
 
 if question:
     response = model.generate_content(question)
-    markdown_response = f"Chatbot -> {to_markdown(response.text)}"
+    markdown_response = f"Chatbot → {response.text}"
     st.markdown(markdown_response)
